808f9dfe8a41bd42e5fdb7c128792bb5e2549fdc
artoria-tsubaki <1248353039@qq.com>
2023-08-14 21:33:42 +0800

    设置初始化默认zoom 移除api响应式方法

--- src/components/Timeline2.vue
+++ src/components/Timeline2.vue
@@ -1,5 +1,4 @@
 <template>
-  
   <Snslist />
   <div id='timeline-embed' style="width: 100%; height: 100%;"></div>
 </template>
@@ -12,16 +11,28 @@ import Snslist from './Snslist.vue';
 import data from '@/assets/data.json'
 import mittBus from '@/utils/mittBus';
 
-onMounted(() => {
-  const timeline = new TL.Timeline('timeline-embed', data)
 
-  timeline.on('change', (id) => {
-    console.log(id);
-  })
+onMounted(() => {
+  const timeline = new TL.Timeline('timeline-embed', data, { initial_zoom: 4 })
+  
+  // timeline.on('loaded', (e) => {
+  //   console.log(e);
+  // })
+  
+  // timeline.on('change', (id) => {
+  //   console.log(id);
+  // })
   
   mittBus.on('goto', (index) => {
     timeline.goTo(index)
   })
+
+  // const timelineResize = () => {
+  //   console.log('function timelineResize');
+  //   timeline && timeline.updateDisplay();
+  // }
+
+  // window.addEventListener("resize", timelineResize);
 })
 </script>
 
